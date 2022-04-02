2f45386cf3b60dc796078c1129529ce5724020c8
gabriel.camps <ext_gabcampsl@falabella.cl>
2022-04-02 18:58:58 -0500

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./Home.css";
+import {Logo} from '../images/Netflix';
+import { ConnectButton, Icon, TabList, Tab } from 'web3uikit';
+
+const Home = () => {
+
+return(
+  <>
+  <div className='logo'>
+    <Logo />
+  </div>
+  <div className='connect'>
+    <Icon fill='#ffffff' size={24} svg="bell" />
+    <ConnectButton />
+  </div>
+  <div className="topBanner">
+     <TabList defaultActiveKey={1} tabStyle="bar">
+      <Tab tabKey={1} tabName="Movies" ></Tab>
+      <Tab tabKey={2} tabName="Series" isDisabled={true}></Tab>
+      <Tab tabKey={3} tabName="My List" ></Tab>
+     </TabList>
+  </div>
+  </>
+)
+}
+
+export default Home;
